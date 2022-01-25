2158162bfb04263eba16b1147530fc97a7c7ccc9
amberwagoner <amberwagoner@outlook.com>
2022-01-24 19:19:25 -0800

    let's see

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+#include <std.io>
+
+/*
+ * main is the entry point and beginning of our code
+ */
+int main(void)
+{
+	/*
+	 * Identify string using char
+	 */
+	char str1[] = "\"Programming is like building a multilingual puzzle";
+
+	/*
+	 * puts will print the previously identified string
+	*/
+	puts(str1);
+
+	return (0);
+}
