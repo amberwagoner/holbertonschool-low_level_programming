41d0b0765fed72cb0fbf4035e249531244420ecc
amberwagoner <amberwagoner@outlook.com>
2022-01-24 20:09:44 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	int printf("with proper grammar, but the outcome is a piece of art, \n");
+	printf("with proper grammer, but the outcome is a piece of art, \n");
 
 	return (0);
 }
