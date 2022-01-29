e95dfb177d3516698e03fa2f8f2d1f0631a136cb
amberwagoner <amberwagoner@outlook.com>
2022-01-28 17:48:36 -0800

    took out ascii

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,13 +14,13 @@ void times_table(void)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			_putchar(44 + '0');
-			_putchar(20 + '0');
+			_putchar(',');
+			_putchar(' ');
 
 			k = (i * j);
 
 			if (k <= 9)
-				_putchar(20 + '0');
+				_putchar(' ');
 			else
 				_putchar((k / 10) + '0');
 
