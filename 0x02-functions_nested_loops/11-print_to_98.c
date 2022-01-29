9c28682b10b3d31fe8853af56046406cc1c4b478
amberwagoner <amberwagoner@outlook.com>
2022-01-28 16:33:51 -0800

    if else if else

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints down or up to 98
+ * @n: number being printed
+ * Return: 0.
+ */
+
+void print_to_98(int n)
+{
+	n = 0;
+
+	if (n < 98)
+	{
+		_putchar(n);
+		_putchar(44);
+		_putchar(' ');
+		++n;
+		if (n == 98)
+			return;
+	}
+	else if (n > 98)
+	{
+		_putchar(n);
+		_putchar(44);
+		_putchar(' ');
+		--n;
+		if (n == 98)
+			return;
+	}
+	else
+		_putchar(n);
+}
