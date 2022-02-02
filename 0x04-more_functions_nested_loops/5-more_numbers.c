52503a0c3f09bc1cccf24f0d8742b44a40f24397
amberwagoner <amberwagoner@outlook.com>
2022-02-01 21:58:31 -0800

    added if loop

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers 0 to 14 ten times,
+ * followed by new line
+ *
+ * Return: 0
+ */
+
+void more_numbers(void)
+{
+	int next = 0;
+	int n;
+
+	for (next = 1; next <= 10; next++)
+	{
+		for (n = 0; n <= 14; n++)
+		{	
+			if (n > 9)
+			{
+				_putchar((n / 10) + '0');
+			}
+			_putchar((n % 10) + '0');
+		}
+	_putchar('\n');
+	}
+}
