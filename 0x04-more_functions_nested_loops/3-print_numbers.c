23eefc4554c55b7466767d97e5eff85c25667785
amberwagoner <amberwagoner@outlook.com>
2022-02-01 16:39:32 -0800

    first

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers 0 to 9 followed by new line
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int _putchar(char);
+	char n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
