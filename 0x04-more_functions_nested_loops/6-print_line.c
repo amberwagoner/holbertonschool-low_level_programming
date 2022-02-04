472704896aa3a4bc8661aefb8f2ae6db89bfd80f
amberwagoner <amberwagoner@outlook.com>
2022-02-04 10:28:04 -0800

    if/for

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - prints a straight line
+ * @n: number of times _ should be printed
+ * Return: 0
+ */
+
+void print_line(int n)
+{
+	int line;
+
+	if (n > 0)
+	{
+		for (line = 0; line < n; line++)
+			_putchar('_');
+	}
+	_putchar('\n');
+}
