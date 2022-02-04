b2e66a1cd773fbbaa61daa2b4e98c41155461dcc
amberwagoner <amberwagoner@outlook.com>
2022-02-03 17:07:06 -0800

    added _putchar.c to gcc, took out str = 0

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,14 +2,13 @@
 
 /**
  * _puts - prints a string
- * _putchar - prints a character
  * @str: string
  * Return: void
  */
 
 void _puts(char *str)
 {
-	for (str = 0; *str != '\0'; str++)
+	for (; *str != '\0'; str++)
 	{
 		_putchar(*str);
 	}
