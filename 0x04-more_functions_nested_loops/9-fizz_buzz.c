417af4e3a222b56fd1d2a8703fba53add6d30942
amberwagoner <amberwagoner@outlook.com>
2022-02-04 17:26:36 -0800

    added main.h...?

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - the infamous FizzBuzz test! numbers 1 to 100 with:
+ * Fizz when n % 3 == 0
+ * Buzz when n % 5 == 0
+ * FizzBuzz when n % 5 == 0 && n % 3 == 0 (or n % 15 == 0)
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n; /* number between 1 and 100 */
+
+	/* for loop, followed by if elseif elseif else */
+	for (n = 1; n <= 100; n++)
+	{
+		if (n % 3 == 0)
+			printf("Fizz ");
+		else if (n % 5 == 0)
+			printf("Buzz ");
+		else if (n % 3 == 0 && n % 5 == 0)
+			printf("FizzBuzz ");
+		else
+			printf("%d ", n);
+	}
+	printf("\n");
+	return (0);
+}
