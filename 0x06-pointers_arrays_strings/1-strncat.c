ce04d3995ce1e2c12449f143ddaa3179419df446
amberwagoner <amberwagoner@outlook.com>
2022-02-07 13:43:00 -0800

    fixed for loop

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -18,11 +18,10 @@ char *_strncat(char *dest, char *src, int n)
 
 	for (j = 0; j < n && src[j] != '\0'; j++)
 	{
-		dest[i] = src[j];
+		dest[j + i] = src[j];
 		j++;
 
 	}
 	dest[j + i] = '\0';
-
 	return (dest);
 }
