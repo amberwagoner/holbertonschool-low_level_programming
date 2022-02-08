f45f331d08ebeb0693de041803866f4053bd60e7
amberwagoner <amberwagoner@outlook.com>
2022-02-08 08:44:51 -0800

    fixed while statement

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,14 +10,14 @@
 int _strcmp(char *s1, char *s2)
 {
 	/* loop until both strings end */
-	while (*s1 != '\0' || *s2 != '\0')
+	while ((*s1 != '\0' || *s2 != '\0') && *s1 == *s2)
 	{
 		s1++;
 		s2++;
-		/* print difference in strings */
-		if (*s1 == *s2)
-			return (0);
-		else
-			return (*s1 - *s2);
 	}
+	/* print difference in strings */
+	if (*s1 == *s2)
+		return (0);
+	else
+		return (*s1 - *s2);
 }
