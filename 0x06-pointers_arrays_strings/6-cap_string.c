e356904667671b33c4220e4b2c3af1ca17a0b400
amberwagoner <amberwagoner@outlook.com>
2022-02-12 16:48:40 -0800

    got rid of spec variable, fixed syntax

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,30 +8,30 @@
 
 char *cap_string(char *s)
 {
-	int str = 0; /* string */
-	int spec = (str - 1); /* special character */
+	int str;
 
-	while (s[str] != '\0')
+	for (str = 0; s[str] != '\0'; str++)
 	{
 		if (s[str] >= 'a' && s[str] <= 'z')
 		{
-		if (s[spec] == ' ' ||
-			s[spec] == '\t' ||
-			s[spec] == '\n' ||
-			s[spec] == ',' ||
-			s[spec] == ';' ||
-			s[spec] == '.' ||
-			s[spec] == '!' ||
-			s[spec] == '?' ||
-			s[spec] == '"' ||
-			s[spec] == '(' ||
-			s[spec] == ')' ||
-			s[spec] == '{' ||
-			s[spec] == '}' ||
+		if (s[str - 1] == ' ' ||
+			s[str - 1] == '\t' ||
+			s[str - 1] == '\n' ||
+			s[str - 1] == ',' ||
+			s[str - 1] == ';' ||
+			s[str - 1] == '.' ||
+			s[str - 1] == '!' ||
+			s[str - 1] == '?' ||
+			s[str - 1] == '"' ||
+			s[str - 1] == '(' ||
+			s[str - 1] == ')' ||
+			s[str - 1] == '{' ||
+			s[str - 1] == '}' ||
 			str == 0)
+			{
 				s[str] = s[str] - 32;
+			}
 		}
-		str++;
 	}
 	return (s);
 }
