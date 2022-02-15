740e2bb767d533bb59dad7870c999dea68b7c8d0
amberwagoner <amberwagoner@outlook.com>
2022-02-15 11:03:53 -0800

    initial files and 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - print the name of the program, followed by new line
+ * @argv: array of pointers to arrays of character objects
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
