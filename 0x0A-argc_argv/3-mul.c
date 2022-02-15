8704f933593d1f57d7d5006dcfb8dca060b46b25
amberwagoner <amberwagoner@outlook.com>
2022-02-15 12:46:04 -0800

    fixed syntax

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,21 +11,19 @@ int main(int argc, char *argv[])
 {
 	int a, b, sum;
 
-	/* get values for a, b, and sum */
-	a = atoi(argv[1]);
-	b = atoi(argv[2]);
-	sum = a * b;
-
 	/* conditions if there aren't two numbers - 3 includes argv[0] */
-	if (argc < 3)
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
+
+	/* get values for a, b, and sum */
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	sum = a * b;
+	
 	/* conditions if there are two numbers */
-	else
-	{
-		printf("%d\n", sum);
-		return (0);
-	}
+	printf("%d\n", sum);
+	return (0);
 }
