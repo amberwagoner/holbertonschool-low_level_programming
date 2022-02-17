e3acd455dc594c60dbcc4cbc800bf01246fa7736
amberwagoner <amberwagoner@outlook.com>
2022-02-16 18:05:21 -0800

    initial files and 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * create_array - creates an array of characters
+ * @size: length of the array
+ * @c: character(s)
+ * Return: pointer to array, NULL if fails
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *str;
+	unsigned int i;
+
+	str = malloc((size + 1) * sizeof(char));
+
+	if (str == NULL || str == 0)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		str[i] = c;
+	return (str);
+}
