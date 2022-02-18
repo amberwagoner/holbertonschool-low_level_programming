ceab23bff657827ab89e5fd22717f31ee9680495
amberwagoner <amberwagoner@outlook.com>
2022-02-17 19:05:08 -0800

    if this works it's a game changer

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,67 @@
+#include "main.h"
+
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+
+/**
+ * _strdup - returns a pointer to newly allocated space in memory,
+ * which contains a copy of the string given as a parameter.
+ * @str: string given as parameter
+ * Return: a pointer to duplicated string if successful,
+ * NULL if not
+ */
+
+char *_strdup(char *str)
+{
+	char *dup; /* duplicate string */
+
+	/* define dup with malloc */
+	dup = malloc(_strlen(str) + 1);
+
+	/* condition if string is NULL */
+	if (str == NULL)
+		return (NULL);
+
+	/* copy string */
+	_strcpy(dup, str);
+
+	return (dup);
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: 0
+ */
+
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		count++;
+	}
+	return (count);
+}
+
+/**
+ * *_strcpy - copies the string pointed to by src
+ * @src: source
+ * @dest: return
+ * Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i]; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
