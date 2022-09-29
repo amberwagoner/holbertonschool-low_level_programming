742ea497a6653d03f931292795f2b162062a8f65
amberwagoner <amberwagoner@outlook.com>
2022-09-29 13:53:46 -0700

    moved function into file

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,24 @@
 #include "holberton.h"
 
 /**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: 0
+ */
+
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		count++;
+	}
+	return (count);
+}
+
+/**
  * _strcat - concatenates two strings
  * @src: source string
  * @dest: destination string
