4101a75ae9defe068a788db2970e8f2d739fc48f
amberwagoner <amberwagoner@outlook.com>
2022-10-24 12:20:19 -0700

    switched if statement

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,5 @@
 #include "dog.h"
-#include <stddef.h>
+#include <stdlib.h>
 
 /**
 * init_dog - initializes dog
@@ -11,10 +11,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL)
-		return;
-
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
