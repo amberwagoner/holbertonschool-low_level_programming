30ff7f6f3aaf39098baef673a2afef0c01c418b0
amberwagoner <amberwagoner@outlook.com>
2022-10-24 12:42:49 -0700

    added libraries to header file

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,4 @@
 #include "dog.h"
-#include <stdio.h>
 
 /**
 * init_dog - initializes dog
@@ -11,11 +10,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d != NULL)
+	if (d)
 	{
 		d->name = name;
 		d->age = age;
 		d->owner = owner;
 	}
-	return;
 }
