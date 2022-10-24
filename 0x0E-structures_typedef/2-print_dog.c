ccca73ca214759ad0593825eb022a58d3774d19d
amberwagoner <amberwagoner@outlook.com>
2022-10-24 14:11:36 -0700

    added Age: and Owner: to else statements

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,12 +13,12 @@ void print_dog(struct dog *d)
 		printf("Name: %s\n", d->name);
 
 	if (d->age == 0)
-		printf("(nil)");
+		printf("Age: (nil)");
 	else
 		printf("Age: %f\n", d->age);
 
 	if (d->owner == NULL)
-		printf("(nil)");
+		printf("Owner: (nil)");
 	else
 		printf("Owner: %s\n", d->owner);
 
