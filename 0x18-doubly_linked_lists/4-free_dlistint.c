69d9d53ac7b0a83ec9fc30496380d05bc3ef5d6a
amberwagoner <amberwagoner@outlook.com>
2022-11-10 12:35:39 -0800

    changed while loop

--- 0x18-doubly_linked_lists/4-free_dlistint.c
+++ 0x18-doubly_linked_lists/4-free_dlistint.c
@@ -7,13 +7,12 @@
 
 void free_dlistint(dlistint_t *head)
 {
-	dlistint_t *temp = head;
+	dlistint_t *temp;
 
-	while (temp->next != NULL)
+	while (head)
 	{
-		temp = temp->next;
-		free(temp->prev);
+		temp = head->next;
+		free(head);
+		head = temp;
 	}
-	free(temp);
-	head = NULL;
 }
