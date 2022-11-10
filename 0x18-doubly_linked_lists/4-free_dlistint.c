6ccbea141158277d38d9c5ac78b351af28ee96ae
amberwagoner <amberwagoner@outlook.com>
2022-11-10 12:30:30 -0800

    Task 4

---
+++ 0x18-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+* free_dlistint - frees a list
+* @head: head pointer
+*/
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp = head;
+
+	while (temp->next != NULL)
+	{
+		temp = temp->next;
+		free(temp->prev);
+	}
+	free(temp);
+}
