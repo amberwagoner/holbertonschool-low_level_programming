b627b1d54e2e5e6654fa86e20278c5a1559dc625
amberwagoner <amberwagoner@outlook.com>
2022-11-15 12:29:35 -0800

    removed NULL from newTable declaration

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,16 +9,18 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i;
-	hash_table_t *newTable = NULL;
+	hash_table_t *newTable;
 
 	newTable = malloc(sizeof(hash_table_t));
 	if (newTable == NULL)
 		return (NULL);
+
 	newTable->size = size;
 	newTable->array = malloc(sizeof(hash_node_t) * size);
 	if (newTable->array == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
 		newTable->array[i] = NULL;
+
 	return (newTable);
 }
